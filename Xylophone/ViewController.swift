55e54d3fd0618abe7b36bc7ab0b260150b14c451
Jagoda Nowak <jn@MacBook-Pro-Jagoda.local>
2020-09-28 21:13:39 +0200

    initialCommit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: (sender.titleLabel?.text)!)
+        //alternative: print(sender.currentTitle)
+        //alternative: print(sender.titleLabel?.text)
+        
+        sender.alpha = 0.5
+        //^ button faded while pressed
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            // ^ button normal after 0.2sec
+        }
+    }
     
-
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play() 
+                
+    }
 }
-
